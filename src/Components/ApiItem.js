aa36c218eaf24f4ecb31724ae6661edc925950f9
Neal Mullen <neal@ashfalldesign.com>
2020-04-03 20:03:15 +0100

    Just testing results from API

--- src/Components/ApiItem.js
+++ src/Components/ApiItem.js
@@ -1,10 +1,12 @@
 
 import React from 'react'
 
-// class ApiItem extends React.Component{
-// }
-
-const ApiItem = () =>
-    <p>TEST</p>
+// Deonstruct the array we passed over form the list component
+const ApiItem =({flight_number, mission_name, launch_date_utc}) => 
+    <li>
+    <span>{flight_number}</span>
+    <span>{mission_name}</span>
+    <span>{launch_date_utc}</span>
+    </li>
 
 export default ApiItem
--- src/Components/ApiItemList.js
+++ src/Components/ApiItemList.js
@@ -6,6 +6,8 @@ class ApiItemList extends React.Component{
     constructor(props){
         super(props);
         this.state ={}
+
+        this.callApi = this.callApi.bind(this);
     }
 
     componentDidMount(){
@@ -13,14 +15,24 @@ class ApiItemList extends React.Component{
     }
 
     callApi(){
-        
+        fetch(
+            "https://api.spacexdata.com/v3/launches?filter=flight_number,mission_name,launch_date_utc&pretty=true")
+            .then((response) => {
+              return response.json();
+            })
+            .then((api) => {
+              this.setState({ data: api });
+            });
     }
-    render(){
-        return(
-            // FOR EACH ITEM IN THE LIST - DO ITEM
-            <ApiItem />
-        )
-    }
+    render() {
+        return (
+          <ol>
+            {(this.state.data || []).map((item, i) => (
+              <ApiItem key={i} {...item} />
+            ))}
+          </ol>
+        );
+      }
 }
 
 export default ApiItemList
